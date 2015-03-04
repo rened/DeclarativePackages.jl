3acbe7e2606b14df2232e20e8dc2355d40822447
Rene Donner <code@donner.at>
2015-03-04 21:09:28 +0100

    fix runtests.jl for PkgEval

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,17 @@ using Base.Test
 
 test(name, r, value) = test(name, r, x->x==value)
 function test(name, r, f::Function)
-	if !f(r)
-		println("Test '$name' failed")
-		@show r
-		exit(1)
-	end
+    a = filter(x->length(x)>7 && x[1:7]!="Cloning", split(r[1], '\n'))
+    a = filter(x->length(x)>4 && x[1:4]!="http", a)
+    a = join(a)
+    r2 = (a,r[2])
+    if !f(r2)
+        println("Test '$name' failed")
+        println("the output was: ---------")
+        @show r
+        println("------ end of output")
+        exit(1)
+    end
 end
 
 
@@ -16,26 +22,26 @@ existinginstallation = exists(decdir)
 
 jdp = Pkg.dir("DeclarativePackages/bin/jdp")
 function runjdp(file)
-	file = Pkg.dir("DeclarativePackages")*"/test/"*file
-	listpackages = Pkg.dir("DeclarativePackages")*"/test/listpackages.jl"
+    file = Pkg.dir("DeclarativePackages")*"/test/"*file
+    listpackages = Pkg.dir("DeclarativePackages")*"/test/listpackages.jl"
 
-	println("Testing $file")
- 	tmp = tempname()
-	run(`touch $tmp`)
-	cp(file, tmp)
-	ENV["DECLARE"] = tmp
-	ENV["DECLARE_VERBOSITY"] = 0
+    println("Testing $file")
+    tmp = tempname()
+    run(`touch $tmp`)
+    cp(file, tmp)
+    ENV["DECLARE"] = tmp
+    ENV["DECLARE_VERBOSITY"] = 0
     r = (readall(`$jdp $listpackages`), readall(tmp))
-	rm(tmp)
-	r
+    rm(tmp)
+    r
 end
 
 ENV["DECLARE_INCLUDETEST"] = ""
-test("empty", runjdp("DECLARE.empty"), x->x[1] == "")
-test("METADATA1", runjdp("DECLARE.METADATA1"), x->x[1] == "")
-test("METADATA2", runjdp("DECLARE.METADATA2"), x->x[1] == "")
-test("JSON1", runjdp("DECLARE.JSON1"), x->x[1]=="JSON 0.3.9\n")
-test("JSON2", runjdp("DECLARE.JSON2"), x->x[1]=="JSON 0.3.7\n")
+test("empty", runjdp("DECLARE.empty"), x->isempty(x[1]))
+test("METADATA1", runjdp("DECLARE.METADATA1"), x->isempty(x[1]))
+test("METADATA2", runjdp("DECLARE.METADATA2"), x->isempty(x[1]))
+test("JSON1", runjdp("DECLARE.JSON1"), x->x[1]=="JSON 0.3.9")
+test("JSON2", runjdp("DECLARE.JSON2"), x->x[1]=="JSON 0.3.7")
 test("HDF51", runjdp("DECLARE.HDF5_1"), x->ismatch(r"HDF5 0\.4\.5",x[1]) && !ismatch(r"DataFrames", x[2]))
 test("HDF52", runjdp("DECLARE.HDF5_2"), x->ismatch(r"HDF5 0\.4\.5",x[1]) && ismatch(r"DeclarativePackages 0\.0\.0-",x[1]))
 test("HDF53", runjdp("DECLARE.HDF5_3"), x->ismatch(r"rened/HDF5",x[2]))
@@ -44,6 +50,6 @@ ENV["DECLARE_INCLUDETEST"] = "true"
 test("HDF55_withtest", runjdp("DECLARE.HDF5_1"), x->ismatch(r"DataFrames",x[1]))
 
 if !existinginstallation
-	run(`chmod -R a+w $decdir`)
-	run(`rm -rf $decdir`)
+    run(`chmod -R a+w $decdir`)
+    run(`rm -rf $decdir`)
 end
