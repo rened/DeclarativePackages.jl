7388f3f44e8fbb0ef710fc45e27eb3ae6c24dbb8
Rene Donner <code@donner.at>
2015-08-30 20:19:04 +0200

    fix isdirty

--- src/DeclarativePackages.jl
+++ src/DeclarativePackages.jl
@@ -55,7 +55,7 @@ function generatespecs()
     osspecific = Any[]
     for pkg in packages
         dir = Pkg.dir(pkg)
-        git = ["git", "--git-dir=$dir/.git"]
+        git = ["git", "-C", dir, "--git-dir=$dir/.git"]
         url = strip(readall(`$git config --get remote.origin.url`))
         metaurl = ""
         try metaurl = strip(readall(Pkg.dir("METADATA")*"/$pkg/url")) catch end
@@ -69,8 +69,12 @@ function generatespecs()
         version = split(strip(readall(`$git name-rev --tags --name-only $commit`)),"^")[1]
         onversion = version != "undefined"
         isahead = ismatch(r"^pinned.*tmp", branch) ? false : !isempty(strip(readall(`$git log $remote/$branch..HEAD`)))
-        if isahead
-            error("Cannot create a jdp declaration from the currently installed packages as '$pkg' has local commits ahead of origin.\nPush those commits, then run 'jdp' again.")
+        status = split(strip(readall(`$git status -s`)), "\n")
+        status = filter(x->!ismatch(r"deps.jl",x) && length(x)>0, status)
+        isdirty = length(status) > 0
+
+        if pkg != "METADATA" && isdirty
+            error("$status -- Cannot create a jdp declaration from the currently installed packages as '$pkg' has local changes.\nPlease commit these changes, then run 'jdp' again.")
         end
         log(2, "generatespecs: pkg: $pkg getsel: $(getsel(pkg)) url: $url")
         list = isempty(getsel(pkg)) ? (url ==  pkg ? metapkgs : giturls) : osspecific 
